f7c2c40bb2f54237ea52a97696712c37b4526853
Tom <tomadesrosiers@gmail.com>
2024-11-11 22:47:50 -0500

    A

---
+++ app/src/main/java/com/cis2818/viewmodeldemo/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.cis2818.viewmodeldemo
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+
+
+
+class MainViewModel: ViewModel() {
+
+    private val rate = 0.74f
+    private var dollarText = ""
+
+    //private var result: Float = 0f
+
+    private var result: MutableLiveData<Float> = MutableLiveData()
+
+    fun setAmount(value: String){
+
+        this.dollarText = value
+        result.value = value.toFloat() * rate
+
+    }
+
+    fun getResult(): MutableLiveData<Float>{
+        return result
+    }
+
+}
